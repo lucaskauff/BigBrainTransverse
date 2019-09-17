d70f194fec61fdf720f8c280bf2c23509ddfd384
Léonard Gombert <epoc4oftime@gmail.com>
2019-09-17 15:58:29 +0200

    AI_Working Movement, Rotation and Spawning

--- BigBrainTransverse2/Assets/1_Scripts/LeonardScripts/SpawnManager.cs
+++ BigBrainTransverse2/Assets/1_Scripts/LeonardScripts/SpawnManager.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 
 public class SpawnManager : MonoBehaviour {
 
+    [SerializeField] List<GameObject> spawnPointsList = new List<GameObject>();
+    [SerializeField] GameObject people;
+    [SerializeField] float numberToSpawn;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            foreach (GameObject citizenSpawner in spawnPointsList)
+            {
+                Instantiate(people, citizenSpawner.transform.position, Quaternion.identity);
+            }
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        
 	}
 }
