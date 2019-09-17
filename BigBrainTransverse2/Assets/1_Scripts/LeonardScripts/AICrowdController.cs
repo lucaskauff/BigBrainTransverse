73bbe8bb76ca5e0c2dd189b7d0170bc51b9f2fa2
Léonard Gombert <epoc4oftime@gmail.com>
2019-09-17 15:08:23 +0200

    Post-Merge

---
+++ BigBrainTransverse2/Assets/1_Scripts/LeonardScripts/AICrowdController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICrowdController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*public static RaycastHit RayCastManger(Vector3 startPosition, Vector3 targetPosition)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(startPosition, targetPosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                float step = speed * Time.deltaTime; // calculate distance to move
+                transform.position = Vector3.MoveTowards(transform.position, hit.collider.transform.position, step);
+            }
+        }
+    }*/
+}
