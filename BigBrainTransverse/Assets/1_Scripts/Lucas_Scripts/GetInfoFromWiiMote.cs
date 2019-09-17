8275f28b54c619973caca870c32f2ff9d362ec1d
Lucas Kauff <l.kauff@rubika-edu.com>
2019-09-17 11:59:09 +0200

    Testing WiiMote Controller Inputs

---
+++ BigBrainTransverse/Assets/1_Scripts/Lucas_Scripts/GetInfoFromWiiMote.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WiimoteApi;
+
+namespace WiimoteApi
+{
+    public class GetInfoFromWiiMote : MonoBehaviour
+    {
+        private Wiimote wiimote;
+
+        void Update()
+        {
+            if (!WiimoteManager.HasWiimote()) { return; }
+
+            wiimote = WiimoteManager.Wiimotes[0];
+
+            if (wiimote.Button.a)
+            {
+                Debug.Log("Button A pressed");
+            }
+        }
+    }
+}
