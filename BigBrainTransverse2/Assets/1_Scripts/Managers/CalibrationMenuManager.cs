6754946534c42d8c62898b3322bfdab8abb9fdd7
Lucas Kauff <l.kauff@rubika-edu.com>
2019-09-20 17:03:37 +0200

    Fitguy integration & calibration menu started !

---
+++ BigBrainTransverse2/Assets/1_Scripts/Managers/CalibrationMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalibrationMenuManager : MonoBehaviour
+{
+    [SerializeField] SuperTextMesh[] playerStatus;
+
+    private void Update()
+    {
+        KinectManager kinectManager = KinectManager.Instance;
+
+        if (kinectManager && kinectManager.IsInitialized())
+        {
+            if (kinectManager.IsUserDetected())
+            {
+                if (kinectManager.IsPlayerCalibrated(kinectManager.Player1ID))
+                {
+                    playerStatus[0].text = "Player 1 Status : Connected !";
+                }
+                else
+                {
+                    playerStatus[0].text = "Player 1 Status : Searching for player...";
+                }
+            }
+        }
+    }
+}
