6fcdbdd477083073996b006efce6bedea057e2e9
azead <saea>
2019-09-28 17:26:18 +0200

    Script Cleanup and Legibility Changes(1)

--- BigBrainTransverse2/Assets/1_Scripts/LeonardScripts/CitizenController.cs
+++ BigBrainTransverse2/Assets/1_Scripts/LeonardScripts/CitizenController.cs
@@ -9,39 +9,34 @@ public class CitizenController : MonoBehaviour
 {
     #region VARIABLE DECLARATIONS
     //GameManager
-    NewGameManager gameManager;
+    [FoldoutGroup("Components")][SerializeField] Collider myCol;
+    [FoldoutGroup("Components")][SerializeField] Animator myAnim;
+    [FoldoutGroup("Components")][SerializeField] Rigidbody rb;
+    [FoldoutGroup("Components")][SerializeField] Renderer cubeRenderer;
+    [FoldoutGroup("Components")][SerializeField] ParticleSystem particleSystem;
 
-    [FoldoutGroup("Internal Components")] [SerializeField] Collider myCol;
-    [FoldoutGroup("Internal Components")] [SerializeField] Animator myAnim;
-    [FoldoutGroup("Internal Components")] [SerializeField] Rigidbody rb;
-    [FoldoutGroup("Internal Components")] [SerializeField] Renderer cubeRenderer;
-    [FoldoutGroup("Internal Components")] [SerializeField] ParticleSystem particleSystem;
+    [FoldoutGroup("UI")][SerializeField] TheBubble bubble;
+    [FoldoutGroup("UI")][SerializeField] Transform reason;
+
+    [FoldoutGroup("Gameplay")][SerializeField] float citizenMoveSpeed;
+    [FoldoutGroup("Gameplay")][SerializeField] float speedDecreaseMultiplier;
+    [FoldoutGroup("Gameplay")][SerializeField] float baseMaxCalorieTol; //maximum calorie tolerance when game starts   
+
+    [FoldoutGroup("Debugging")] public bool isScientist;
+    [FoldoutGroup("Debugging")][SerializeField] bool hitWall = false;
+    [FoldoutGroup("Debugging")][SerializeField] bool isHitByEnergy = false; 
+    [FoldoutGroup("Debugging")][SerializeField] bool isDead = false;
+    [FoldoutGroup("Debugging")][SerializeField] float currentMaxCalorieTol; //maximum calorie tolerance at any given time
+    [FoldoutGroup("Debugging")][SerializeField] float currentCalories;
+    [FoldoutGroup("Debugging")][SerializeField] float intervalToRandomizeRotation;
+    [FoldoutGroup("Debugging")][SerializeField] int greasyHits = 0;
+    [FoldoutGroup("Debugging")][SerializeField] int sweetHits = 0;
+    
+    NewGameManager gameManager;
+    FoodData foodData;
     Stopwatch timer = new Stopwatch();
     Vector3 baseRotationVector;
     Vector3 theCamera;
-
-    [FoldoutGroup("Objects to serialize")] [SerializeField] TheBubble bubble;
-    [FoldoutGroup("Objects to serialize")] [SerializeField] Transform reason;
-
-    [FoldoutGroup("Debug Variables")] public bool isScientist;
-
-    [FoldoutGroup("Debug Variables")] [SerializeField] float currentCalories;
-    [FoldoutGroup("Debug Variables")] [SerializeField] float currentMaxCalorieTol; //maximum calorie tolerance at any given time
-    bool isHitByEnergy = false;
-
-    [FoldoutGroup("Internal Variables")] bool isDead = false;
-    [FoldoutGroup("Internal Variables")] [SerializeField] float baseMaxCalorieTol; //maximum calorie tolerance when game starts
-    [FoldoutGroup("Internal Variables")] [SerializeField] float citizenMoveSpeed;
-    [FoldoutGroup("Internal Variables")] [SerializeField] float intervalToRandomizeRotation;
-    [FoldoutGroup("Internal Variables")] [SerializeField] float speedDecreaseMultiplier;
-    float step;
-    FoodData foodData;
-
-    [FoldoutGroup("Food Hit Values")] [SerializeField] int greasy = 0;
-    [FoldoutGroup("Food Hit Values")] [SerializeField] int sweet = 0;
-
-    //Components
-    
     #endregion
 
     #region //BASE UNITY CALLBACKS
@@ -71,8 +66,7 @@ public class CitizenController : MonoBehaviour
         Movement();
 
         timer.Start();
-
-        if (timer.Elapsed.TotalSeconds >= intervalToRandomizeRotation)
+        if (timer.Elapsed.TotalSeconds >= intervalToRandomizeRotation && !hitWall)
         {
             RandomizeRotation();
             timer.Stop();
@@ -90,9 +84,10 @@ public class CitizenController : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         //use to detect collision against wall and change the facing direction
-        if (collision.gameObject.tag == "Walls")
+        if (collision.gameObject.tag == "Walls" || collision.gameObject.tag == "Buildings")
         {
-            RandomizeRotation();
+            hitWall = true;
+            WallHitRotation();
         }
 
         if (collision.gameObject.tag == "Food")
@@ -105,12 +100,12 @@ public class CitizenController : MonoBehaviour
                 case FoodType.greasy:
                     GreaseEffect(foodData.CalorieGainOnHit);
                     cubeRenderer.material.SetColor("_Color", Color.red);
-                    greasy++;
+                    greasyHits++;
                 break;
                 case FoodType.sweet:
                     SweetEffect(foodData.CalorieGainOnHit, foodData.CalorieToleranceDecrease);
                     cubeRenderer.material.SetColor("_Color", Color.blue);
-                    sweet++;
+                    sweetHits++;
                 break;
                 case FoodType.energy:
                     EnergyEffect(foodData.CalorieGainOnHit, foodData.CalorieGainOverTime);
@@ -122,35 +117,6 @@ public class CitizenController : MonoBehaviour
             if(isScientist) UnityEngine.Debug.Log("You just hit a scientist");//AnimationManager.DeathCamManager(this.gameObject, "Scientist");
         }
     }
-
-    /*void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.tag == "Food")
-        {
-            myAnim.SetTrigger("Hit");
-
-            switch (collision.gameObject.GetComponent<FoodBehavior>().foodData.FoodType)
-            {
-                case FoodType.greasy:
-                    GreaseEffect(foodData.CalorieGainOnHit);
-                    cubeRenderer.material.SetColor("_Color", Color.red);
-                    greasy++;
-                break;
-                case FoodType.sweet:
-                    SweetEffect(foodData.CalorieGainOnHit, foodData.CalorieToleranceDecrease);
-                    cubeRenderer.material.SetColor("_Color", Color.blue);
-                    sweet++;
-                break;
-                case FoodType.energy:
-                    EnergyEffect(foodData.CalorieGainOnHit, foodData.CalorieGainOverTime);
-                    cubeRenderer.material.SetColor("_Color", Color.green);
-                    isHitByEnergy = true;
-                break;
-            }
-
-            if(isScientist) UnityEngine.Debug.Log("You just hit a scientist");//AnimationManager.DeathCamManager(this.gameObject, "Scientist");
-        }
-    }*/
 
     void OnCollisionExit(Collision collision)
     {   
@@ -170,18 +136,22 @@ public class CitizenController : MonoBehaviour
 
     void Movement()
     {
-        step = citizenMoveSpeed * Time.deltaTime; // calculate distance to move
+        float step = citizenMoveSpeed * Time.deltaTime; // calculate distance to move
         rb.MovePosition(transform.position + transform.forward * step);
     }
 
     void RandomizeRotation()
     {
         baseRotationVector = transform.rotation.eulerAngles;
-        //transform.rotation = Quaternion.Euler(baseRotationVector.x, Random.Range(210, 330), baseRotationVector.z);
-        
-        transform.rotation = Quaternion.Euler(baseRotationVector.x, Random.Range(120, 240), baseRotationVector.z);
-        //intervalToRandomizeRotation = Random.Range(0f, 1f);
-        intervalToRandomizeRotation = 2f;
+        transform.rotation = Quaternion.Euler(baseRotationVector.x, Random.Range(-90, 90), baseRotationVector.z);
+        intervalToRandomizeRotation = Random.Range(1f, 3f);
+    }
+
+    void WallHitRotation()
+    {
+        baseRotationVector = transform.rotation.eulerAngles;        
+        transform.rotation = Quaternion.Euler(baseRotationVector.x, Random.Range(200, 160), baseRotationVector.z);
+        hitWall = false;
     }
 
     void GreaseEffect(int caloriesGained)
@@ -218,8 +188,8 @@ public class CitizenController : MonoBehaviour
         myCol.enabled = false;
         rb.constraints = RigidbodyConstraints.FreezeAll;
         
-        if (greasy > sweet) myAnim.SetInteger("DeadType", 1);
-        else if (greasy < sweet) myAnim.SetInteger("DeadType", 2);
+        if (greasyHits > sweetHits) myAnim.SetInteger("DeadType", 1);
+        else if (greasyHits < sweetHits) myAnim.SetInteger("DeadType", 2);
         else if (isHitByEnergy) myAnim.SetInteger("DeadType", 3);
 
         if (!gameManager.isDeathAnimOnGoing)
@@ -227,8 +197,8 @@ public class CitizenController : MonoBehaviour
             bubble.transform.LookAt(theCamera);
             reason.LookAt(theCamera);
             reason.localPosition = new Vector3(reason.localPosition.x, reason.localPosition.y, reason.localPosition.z+0.01f);
-            if (greasy > sweet) bubble.whichDeath = 0;
-            else if (greasy < sweet) bubble.whichDeath = 1;
+            if (greasyHits > sweetHits) bubble.whichDeath = 0;
+            else if (greasyHits < sweetHits) bubble.whichDeath = 1;
             else if(isHitByEnergy) bubble.whichDeath = 2;
             bubble.GetComponent<Animator>().SetTrigger("FadeIn");
         }
